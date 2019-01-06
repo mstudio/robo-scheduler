f36ed800411f61bc47ebc969df6c81abd803d2f1
Alex Motzenbecker <alex@mstudio.com>
2019-01-05 21:24:31 -0500

    Code cleanup.

--- robo-scheduler.ino
+++ robo-scheduler.ino
@@ -21,12 +21,8 @@ RTC_DS3231 rtc;
 Adafruit_7segment matrix = Adafruit_7segment();
 
 const char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-<<<<<<< HEAD
 
-#define NUM_ALARMS 3;
-=======
 #define NUM_ALARMS 3
->>>>>>> cde471eec2c0b4a8bd1666b0e381069584f4d4e5
 
 const int times[NUM_ALARMS][2] = {{10, 3}, {20, 51}, {15, 10}};
 
@@ -70,116 +66,7 @@ void setup () {
 #endif
   matrix.begin(0x70);
 
-  // Audio Setup
-  Serial.print(F("Initializing Emic..."));
-  pinMode(emic2RxPin, INPUT);
-  pinMode(emic2TxPin, OUTPUT);
-  emic2Serial.begin(9600);
-  emic2TtsModule.init();
-  emic2TtsModule.setVolume(10);
-  emic2TtsModule.setWordsPerMinute(1000);
-  emic2TtsModule.setVoice(9);
-  //emic2TtsModule.playSingingDemo();
-  //emic2TtsModule.say("Alex go do somthing you hate being miserble bilds character ");
-  emic2TtsModule.say("calvin go do somthing you hate. being miserble builds character ");
-  Serial.print(F("OK"));
-
-}
-
-/**gdxdsdsf
-   converts hours or minutes to the correct digit for specified index
-   if isHour === true, converts 24 hour time to standard time
-   example (hour): 21 returns 2 (for index 0) returns 1 (for index 1)
-   example (minute): 30 returns 3 (for index 0) returns 1 (for index 1)
-*/
-int convertDigits(int digits, int index, bool isHour) {
-
-  // convert from 24 hour to standard time
-  if (isHour && digits > 12) {
-    digits = digits - 12;
-  }
-  // convert midnight (00) to 12
-  if (isHour && digits == 0) {
-    digits = 12;
-  }
-
-  // convert to string to check length and charAt
-  String timeString = String(digits);
-
-  // prepend with "0" if the hour or minute is a single digit
-  if (timeString.length() < 2) {
-    timeString = "0" + timeString;
-  }
-
-  // get the character at the specified index
-  char indexChar = timeString.charAt(index);
-
-  // convert the character to string and then integer
-  String indexString = String(indexChar);
-  int output = indexString.toInt();
-
-  return output;
-}
-
-/**
-   Logs time to serial monitor, e.g.:
-   2018/12/29 (Saturday) 21:48:40
-*/
-void logTime(DateTime now) {
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-}
-
-/**
-   Writes the current hour and minutes in standard format on LED
-*/
-void writeTimeToLED(DateTime now) {
-  int h0 = convertDigits(now.hour(), 0, true);
-  int h1 = convertDigits(now.hour(), 1, true);
-
-  int m0 = convertDigits(now.minute(), 0, false);
-  int m1 = convertDigits(now.minute(), 1, false);
-
-  matrix.writeDigitNum(0, h0, true);
-  matrix.writeDigitNum(1, h1, true);
-  matrix.drawColon(true);
-  matrix.writeDigitNum(3, m0, true);
-  matrix.writeDigitNum(4, m1, true);
-  matrix.writeDisplay();
-}
-
-/**
- * Pronounce the actual message audio using emic2 module.
- * Then wait 1 minute so that the message does not repeat on the next loop.
- */
-void sayMessage(String message) {
-  Serial.print("say message: ");
-  Serial.print(message);
-  emic2TtsModule.say(message);
-
-  delay(60000);
-}
-
-/**
- * Turns current hour and minute into text, e.g. 17:03 -> "5 oh 3";
- */
-String getTimeMessage(DateTime now) {
-  const String messageHour = (now.hour() > 12) ? String(now.hour() - 12) : String(now.hour());
-  const String messageMin = (now.minute() < 10) ? "oh " + String(now.minute()) : String(now.minute());
-  const String timeMessage = "It is" + messageHour + " " + messageMin + ".";
-  return timeMessage;
+  setupAudio();  
 }
 
 /**
